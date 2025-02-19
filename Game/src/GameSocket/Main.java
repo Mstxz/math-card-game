e2f1005878b79b199a34709dcd57419f132088b9
Wissanupong-Chanliem <wissanupongdev@gmail.com>
2025-02-19 22:11:35 +0700

    Testing Public IP Connection

--- Game/src/GameSocket/Main.java
+++ Game/src/GameSocket/Main.java
@@ -3,13 +3,14 @@ package GameSocket;
 public class Main {
     public static void main(String[] args) {
         Room r = new Room(2);
-        Client c1 = new Client(r.getPrivateIp(),5000);
-        Client c2 = new Client(r.getPrivateIp(),5000);
+        System.out.println(r.getPublicIp());
+        //Client c1 = new Client(r.getPrivateIp(),5000);
+        //Client c2 = new Client(r.getPrivateIp(),5000);
         try{
-            c1.send("Hello");
-            r.cleanUp();
-            c1.close();
-            c2.close();
+            //c1.send("Hello");
+            //r.cleanUp();
+            //c1.close();
+            //c2.close();
 
         }
         catch (Exception e){
