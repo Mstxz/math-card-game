1cb692e2d354fe9b6b94e365c24236a07d037673
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-03-14 11:20:44 +0700

    update result text in ShowResult component

--- Game/src/GUI/Component/ResultShow.java
+++ Game/src/GUI/Component/ResultShow.java
@@ -13,22 +13,43 @@ public class ResultShow extends JPanel {
     private JLabel resultText;
     public ResultShow(boolean isWin){
         banner = new JPanel();
-        resultText = new JLabel();
+        final String text;
         if (isWin){
-            resultText.setText("Victory");
+            text = "Victory";
+
         }
         else {
-            resultText.setText("Defeat");
+            text = "Defeat";
+
         }
-        resultText.setFont(SharedResource.getCustomSizeFont(64));
+        JLabel bigText = new JLabel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setFont(SharedResource.getCustomSizeFont(100));
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                int x = 35;
+                int y = 90;
+                g2d.setColor(SharedResource.SIAMESE_BRIGHT);
+                for(int dx = -5 ;dx <= 5 ;dx++){
+                    for(int dy = -5 ;dy <= 5 ;dy++){
+                        g2d.drawString(text, x+dx,y+dy);
+                    }
+                }
+                g2d.setColor(SharedResource.SIAMESE_LIGHT);
+                g2d.drawString(text, x,y);
+                System.out.println(x+ ", " + y);
+            }
+        };
+
         banner.setBackground(SharedResource.SIAMESE_DARK);
         banner.setBorder(new MatteBorder(2,0,2,0,SharedResource.SIAMESE_BRIGHT));
         banner.setBounds(0,Router.getMainFrame().getHeight()/2-150,Router.getMainFrame().getWidth(),300);
-        resultText.setBounds((Router.getMainFrame().getWidth()-400)/2,banner.getY()-50,400,100);
-        resultText.setHorizontalAlignment(SwingConstants.CENTER);
-        System.out.println(resultText);
+        bigText.setBounds((Router.getMainFrame().getWidth()-400)/2,banner.getY()-50,400,200);
+        //System.out.println(resultText);
         this.setLayout(null);
-        this.add(resultText);
+        this.add(bigText);
         this.add(banner);
 
         this.setOpaque(false);
