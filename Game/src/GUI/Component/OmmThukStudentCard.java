ae8057ec7f50541762651546457ff23e6c553793
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-04-01 23:09:49 +0700

    add: ommthuk's certified signature

--- Game/src/GUI/Component/OmmThukStudentCard.java
+++ Game/src/GUI/Component/OmmThukStudentCard.java
@@ -1,6 +1,7 @@
 package GUI.Component;
 
 import GUI.Router;
+import GUI.Setting.Controller.AchievementProfile;
 import GUI.Setting.UserPreference;
 import utils.SharedResource;
 
@@ -11,13 +12,16 @@ import static utils.ResourceLoader.loadPicture;
 
 public class OmmThukStudentCard extends JPanel {
     private JLabel ProfilePicture;
+    private JLabel OmmThukCertifiedPicture;
     private final JLabel cardName = new JLabel("OMM THUK's Student ID Card");
     private JLabel userNameLabel;
     private JPanel userInfoPanel;
     private JPanel statsPanel;
     private JPanel nameInfo;
     private ImageIcon image;
+    private ImageIcon OmmThukCertified;
     private String profilePicturePath;
+    private AchievementProfile achievementProfile = new AchievementProfile();
 
     private StudentCardInformation username;
     private StudentCardInformation playBotCount;
@@ -29,12 +33,26 @@ public class OmmThukStudentCard extends JPanel {
         super();
         this.profilePicturePath = path;
         this.image = loadPicture(getProfileURL(), 230, 230);
+        OmmThukCertified = loadPicture("assets/OmmThuk_Certified", 458/2, 278/2);
+        OmmThukCertifiedPicture = new JLabel(OmmThukCertified);
 
         username = new StudentCardInformation(new Dimension(600, 230), "NAME", name);
         playBotCount = new StudentCardInformation(new Dimension(200, 150), "PLAY BOT", String.format("%d", UserPreference.getInstance().getWinStat().getPlay()));
         winBotCount = new StudentCardInformation(new Dimension(200, 150), "WIN BOT", String.format("%d",UserPreference.getInstance().getWinStat().getWin()));
         loseBotCount = new StudentCardInformation(new Dimension(200, 150), "LOSE BOT", String.format("%d",UserPreference.getInstance().getWinStat().getLose()));
 
+        /*extra declare*/
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(SharedResource.SIAMESE_LIGHT);
+        bottomPanel.setLayout(new BorderLayout());
+        bottomPanel.setPreferredSize(new Dimension(1000, 300));
+        JPanel userCertifiedPanel = new JPanel();
+        userCertifiedPanel.setBackground(SharedResource.SIAMESE_LIGHT);
+        userCertifiedPanel.setLayout(new BorderLayout());
+        userCertifiedPanel.setPreferredSize(new Dimension(300, 300));
+        if(achievementProfile.isWinOmmThuk()){
+            userCertifiedPanel.add(OmmThukCertifiedPicture, BorderLayout.CENTER);
+        }
         /*set up*/
         setBorder(new RoundBorder(SharedResource.SIAMESE_DARK, SharedResource.SIAMESE_LIGHT, 3, 100));
         setPreferredSize(new Dimension(1150, 660));
@@ -58,7 +76,7 @@ public class OmmThukStudentCard extends JPanel {
 
         /*Stats Info*/
         statsPanel = new JPanel();
-        statsPanel.setPreferredSize(new Dimension(1000, 300));
+        statsPanel.setPreferredSize(new Dimension(700, 300));
         statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.X_AXIS));
         statsPanel.setBackground(SharedResource.SIAMESE_LIGHT);
 
@@ -69,9 +87,12 @@ public class OmmThukStudentCard extends JPanel {
         statsPanel.add(loseBotCount);
         statsPanel.add(Box.createHorizontalStrut(20));
 
+        bottomPanel.add(statsPanel, BorderLayout.WEST);
+        bottomPanel.add(userCertifiedPanel, BorderLayout.EAST);
+
         add(cardName, BorderLayout.NORTH);
         add(userInfoPanel, BorderLayout.CENTER);
-        add(statsPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.CENTER);
     }
 
     public String getProfileURL(){
